c76792dace7472204dca2a321756732c1e44b659
Tyler Diaz <tyler01.28@hotmail.com>
2024-03-07 08:39:00 -0400

    add Date

---
+++ conceptosAvanzados/src/Date/EjemploJavaUtilCalendar.java
@@ -0,0 +1,11 @@
+package org.aguzman.Date;
+
+import java.util.Calendar;
+
+public class EjemploJavaUtilCalendar {
+
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+    }
+}
---
+++ conceptosAvanzados/src/Date/EjemploJavaUtilDate.java
@@ -0,0 +1,19 @@
+package org.aguzman.Date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EjemploJavaUtilDate {
+
+    public static void main(String[] args) {
+
+        Date fecha = new Date();
+        System.out.println("Esta es la fecha actual: " +  fecha);
+
+        //Modificar formato de fecha
+        SimpleDateFormat df = new SimpleDateFormat("EEEE dd 'de' MM, yyyy");
+
+        String sdf = df.format(fecha);
+        System.out.println( sdf );
+    }
+}
